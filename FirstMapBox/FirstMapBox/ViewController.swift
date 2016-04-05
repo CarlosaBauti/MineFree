dae6fc3e2e193a0912f8e8b31fb4b63703aa8291
Carlos Bautista <cbi210@nyu.edu>
2016-04-05 13:48:07 -0400

    clickable annotation added

--- FirstMapBox/FirstMapBox/ViewController.swift
+++ FirstMapBox/FirstMapBox/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import Mapbox
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mapView: MGLMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 40.693364, longitude: -73.985715)
+        point.title = "Initial Position"
+        point.subtitle = " 2 MetroTech, NY 11210"
+        
+        mapView.addAnnotation(point)
     }
-
+    
+    func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always try to show a callout when an annotation is tapped.
+        return true
+    }
+    
+    // Note: You can remove this method, which lets you customize low-memory behavior.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
