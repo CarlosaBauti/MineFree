be93d1858ff5d230aa86e84007daf57c02e6d4dd
Carlos Bautista <cbi210@nyu.edu>
2016-04-22 12:28:47 -0400

    solved minor orientation issue

---
+++ MineSafe/FirstMapBox/DestinationSearchBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DestinationSearchBarViewController.swift
+//  MineSafe
+//
+//  Created by Robert Hieger on 4/19/16.
+//  Copyright © 2016 cbautista. All rights reserved.
+//
+
+import UIKit
+
+class DestinationSearchBarViewController: UISearchBar,
+        UISearchBarDelegate                                 {
+    
+    
+    
+}   // end DestinationSearchBarViewController: UISearchBar,
+    //       UISearchBarDelegate
