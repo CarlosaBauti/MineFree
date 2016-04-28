e3d377b49f4d8bfaeda4636b767bac5b34c744ef
Bobby <rhieger@nyc.rr.com>
2016-04-28 14:56:43 -0400

    Added DestinationItem Class for Use with Table Cells

---
+++ MineSafe/FirstMapBox/DestinationItem.swift
@@ -0,0 +1,47 @@
+//
+//  DestinationItem.swift
+//  MineSafe
+//
+//  Created by Robert Hieger on 4/28/16.
+//  Copyright © 2016 cbautista. All rights reserved.
+//
+
+import UIKit
+
+/*****************************************************
+ *                                                   *
+ * DestinationItem Class:                            *
+ *                                                   *
+ * This class defines the data that will be fed to   *
+ * the table cells in DestinationViewController. The *
+ * structure for this data is defined in the         *
+ * Destination Class that contains an array of       *
+ * Dictionaries with Key-Value pairs of:             *
+ *                                                   *
+ *      name: String                                 *
+ *  latitude: Double                                 *
+ * longitude: Double                                 *
+ *                                                   *
+ *****************************************************
+*/
+
+class DestinationItem: NSObject   {
+    
+    // MARK: - Properties
+    
+        var destName: String
+    
+    
+    // MARK: - Designated Initializer
+    
+    init(destName: String)  {
+        
+        self.destName = destName
+        
+        // Call super implementation of init().
+        
+        super.init()
+        
+    }   // end init(destName: String)
+    
+}   // end DestinationItem
