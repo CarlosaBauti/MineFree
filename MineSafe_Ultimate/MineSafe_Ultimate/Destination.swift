4d38a679c97339c9f986b1ba3f3e9612ecc7f257
Carlos Bautista <cbi210@nyu.edu>
2016-05-03 00:44:28 -0400

    Intial Commit to this branch

---
+++ MineSafe_Ultimate/MineSafe_Ultimate/Destination.swift
@@ -0,0 +1,60 @@
+//Destination.swift
+//  MineSafe
+//
+//  Created by Robert Hieger on 4/24/16.
+//  Copyright © 2016 cbautista. All rights reserved.
+//
+import UIKit
+
+class Destination   {
+    
+    // MARK: - Properties
+    
+    // Define dictionary to hold coordinates of destinations
+    // chosen by user.
+    
+    var safeDestination = [
+    
+        [     "name":     "The Picnic House",
+          "latitude":     40.665531,
+         "longitude":     -73.971516],
+    
+        [     "name":     "Prospect Park Zoo",
+          "latitude":     40.665334,
+         "longitude":     -73.965526],
+    
+        [     "name":     "LeFrak Center at Lakeside",
+          "latitude":     40.657069,
+         "longitude":     -73.963887],
+    
+        [     "name":     "Prospect Park Baseball Field 7",
+          "latitude":     40.662098,
+         "longitude":     -73.974828],
+    
+        [     "name":     "Brooklyn Botanic Garden",
+          "latitude":     40.66889,
+         "longitude":     -73.965071],
+    
+        [     "name":     "Lincoln Road Playground",
+          "latitude":     40.661167,
+         "longitude":     -73.96379],
+    
+        [     "name":     "Prospect Park Boathouse",
+          "latitude":     40.660808,
+         "longitude":     -73.965139],
+    
+        [     "name":     "Long Meadow Ballfields",
+          "latitude":     40.661528,
+         "longitude":     -73.974345],
+    
+        [     "name":     "Prospect Park Dog Beach",
+          "latitude":     40.662405,
+         "longitude":     -73.972135],
+    
+        [     "name":     "Quaker Cemetery",
+          "latitude":     40.659294,
+         "longitude":     -73.972661]
+    
+        ]   // end safeDestination
+    
+}   // end Destination
